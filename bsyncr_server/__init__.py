eb2721c4fdd420ee809db8bda8dfaef7aaa1f669
Damon Haley <damon.haley@nrel.gov>
2025-04-09 12:39:21 -0600

    add healthcheck.

--- bsyncr_server/__init__.py
+++ bsyncr_server/__init__.py
@@ -80,3 +80,6 @@ def root():
 
         zip_file.seek(0)
         return send_file(zip_file, attachment_filename='bsyncr.zip')
+@app.route("/health", methods=["GET"])
+def health_check():
+    return jsonify({"status": "healthy"}), 200
